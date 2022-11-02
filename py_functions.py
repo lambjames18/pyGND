GND calculation works now, tested on two individual points within a dataset. Need to test on a volume. There is odd behavior where we have to invert tje kappaSR array in order to get the correct output.

py_functions.py
@@ -375,25 +375,20 @@ def GND(coords, featIDs, GAO, cs, symOp, spacing, A, B, burgers):
         
         # Convert Kappa to crystal coordinates since dislocations are
         # described in crystal coordinates
-        kappaSRprime = GAO[:, :, x1, x2, x3].T.dot(kappaSR.dot(GAO[:, :, x1, x2, x3]))
-        print("kappaSRprime\n",kappaSRprime)
+        kappaSRprime = GAO[:, :, x1, x2, x3].T.dot(kappaSR[:, ::-1]).dot(GAO[:, :, x1, x2, x3])
         
         # Calculate Nye Tensor (alpha) from curvature kappa  
         alphaSR = kappaSRprime.T - np.trace(kappaSRprime)
-        print("alphaSR\n",alphaSR)
 
         #function used to determine a total value of gnd density at one particular material point
 
         # determine dislocation densities (dd -> rho) from misorientations
         ddSR = L2_SparseV2(alphaSR, cs, A, B, burgers)
-        print("ddSR\n",ddSR)
 
         # determine total gnd density to be sum of dislocation density across all
         # slip systems
         totalGNDdensitySR = np.abs(ddSR).sum()
         ddSR = np.abs(ddSR).T
-        print("totalGNDdensitySR\n",totalGNDdensitySR)
-        print("ddSR\n",ddSR)
         
         # repeat misorientation calculations using LR approach
         # it is just zero (not sure why, but it was commented out in original code)

test.py
@@ -1,10 +1,7 @@
 import numpy as np
 
-for i in range(10):
-    for j in range(20):
-        for k in range(30):
-            print(i, j, k)
-            if i == 1 and j == 2 and k == 3:
-                break
+a = np.array([[1,2,3], [2,3,4], [5,6,7]])
+b = np.array([[3,2,1], [4,3,2], [7,6,5]])
 
-print("Broke")
+print(a.T.dot(b).dot(a))
+print(a.T.dot(b))