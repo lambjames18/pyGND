4c9fec741fa6f99860ab1d4ea8ea407602a88fb9
James Lamb <lambjames18@gmail.com>
2025-09-24 13:08:44 -0700

    Fixed resolution issue.

--- TriBeam_run.py
+++ TriBeam_run.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     # cell_data_path = "DataStructure/ImageGeometry/Cell Data"
     # spacing_path = "DataStructure/ImageGeometry"
     # path = "E:/CoNi90-thin/old_d3d/CoNi90-thin.dream3d"
-    path = "C:/Users/Pollock-GPU/Documents/Tri Beam/CoNi90_Wrought/CoNi90_Wrought_aligned_mut_final.dream3d"
+    path = "/Users/jameslamb/Documents/research/data/CoNi90-thin/CoNi90-thin_merged.dream3d"
     cell_data_path = "DataContainers/ImageDataContainer/CellData"
     spacing_path = "DataContainers/ImageDataContainer/_SIMPL_GEOMETRY/SPACING"
     dream3d_nx = False
@@ -23,17 +23,16 @@ if __name__ == "__main__":
     # Burgers vector magnitude in m
     burgers = 2.48e-10
     # Number of CPU cores to use
-    n_cpus = 4
+    n_cpus = 10
     # Crystal structure, 1 = FCC, 2 = BCC, 3 = HCP
-    cs = 1 
+    cs = 1
     # "l2" or "l1" (where l1 is the absolute value); l2 is faster, l1 may be more accurate
     minimization = "l2"
     progress_bar = True
     # How many data points to process in one chunk (decrease if memory issues)
     chunk_size = 1000
-
-
-
+    # Spacing units
+    units = "um"
 
     euler, ids = utils.read_dream3d(
         path,
@@ -43,6 +42,23 @@ if __name__ == "__main__":
     spacing = utils.read_dream3d_spacing(
         path, spacing_path=spacing_path, dream3d_nx=dream3d_nx
     )
+    if units == "nm" or units == "nanometer" or units == "nanometers":
+        spacing *= 1e-9
+    elif (
+        units == "um"
+        or units == "micron"
+        or units == "microns"
+        or units == "micrometer"
+        or units == "micrometers"
+        or "µm"
+    ):
+        spacing *= 1e-6
+    elif units == "mm" or units == "millimeter" or units == "millimeters":
+        spacing *= 1e-3
+    elif units == "m" or units == "meter" or units == "meters":
+        pass
+    else:
+        raise ValueError("units must be one of 'nm', 'um', 'mm', or 'm'")
 
     calc = True
     if calc:
@@ -92,8 +108,6 @@ if __name__ == "__main__":
     # del mis1, mis2  # Free memory
     # print("mis:", mis.shape)
 
-
-
     # Save the results back to the DREAM3D file
     import h5py
 
@@ -103,22 +117,28 @@ if __name__ == "__main__":
             dd[minimization].sum(axis=0).reshape(mis.shape[1:] + (1,))
         )
     except Exception as e:
-        print("Failed to write GND data to DREAM3D file. Check that a 'GND' data array exists in the DREAM3D file.")
+        print(
+            "Failed to write GND data to DREAM3D file. Check that a 'GND' data array exists in the DREAM3D file."
+        )
         print(e)
     try:
         h5[f"{cell_data_path}/FDM_avg"][...] = mis.mean(axis=0).reshape(
             mis.shape[1:] + (1,)
         )
     except Exception as e:
-        print("Failed to write FDM_avg data to DREAM3D file. Check that a 'FDM_avg' data array exists in the DREAM3D file.")
+        print(
+            "Failed to write FDM_avg data to DREAM3D file. Check that a 'FDM_avg' data array exists in the DREAM3D file."
+        )
         print(e)
     try:
         h5[f"{cell_data_path}/FDM_max"][...] = mis.max(axis=0).reshape(
             mis.shape[1:] + (1,)
         )
     except Exception as e:
-        print("Failed to write FDM_max data to DREAM3D file. Check that a 'FDM_max' data array exists in the DREAM3D file.")
+        print(
+            "Failed to write FDM_max data to DREAM3D file. Check that a 'FDM_max' data array exists in the DREAM3D file."
+        )
         print(e)
-    
+
     h5.close()
     print("Finished")
