a69246cc6bc39578344a714ca1a4c29578395dcc
mvailhe <123012022+mvailhe@users.noreply.github.com>
2025-09-29 13:07:06 -0700

    Creating a more stable version.

--- TriBeam_run.py
+++ TriBeam_run.py
@@ -1,6 +1,6 @@
 # GND calculations in a 3D TriBeam dataset
 # Author: James Lamb (GND calculations originally from Wyatt Witzen)
-# All parameters should be stated within the config.ini file
+# Designed to read in and write out to DREAM3D files, but can be adapted for other formats
 
 import os
 import numpy as np
@@ -10,30 +10,34 @@ import py_functions as pf
 import GND
 
 
+#############################################
+path = "F:/Haydn/CoNi90_Top/CoNi90_Top_Final.dream3d"
+cell_data_path = "DataContainers/ImageDataContainer/CellData"
+spacing_path = "DataContainers/ImageDataContainer/_SIMPL_GEOMETRY/SPACING"
+dream3d_nx = False
+
+# Burgers vector magnitude in m
+burgers = 2.48e-10
+# Number of CPU cores to use
+n_cpus = 30
+# Crystal structure, 1 = FCC, 2 = BCC, 3 = HCP
+cs = 1
+# "l2" or "l1" (where l1 is the absolute value); l2 is faster, l1 may be more accurate
+minimization = "l2"
+# Whether to show a progress bar
+progress_bar = True
+# How many data points to process in one chunk (decrease if memory issues)
+chunk_size = 1000
+# Spacing units, ensures that the spacing is converted to meters correctly
+units = "um"
+# Whether to perform the calculation (True) or read in previously calculated data from .npy files (False)
+calc = True
+#############################################
+
+
 if __name__ == "__main__":
-    # path = "/Users/jameslamb/Documents/research/data/Wrought-DIC/EBSD.dream3d"
-    # cell_data_path = "DataStructure/ImageGeometry/Cell Data"
-    # spacing_path = "DataStructure/ImageGeometry"
-    # path = "E:/CoNi90-thin/old_d3d/CoNi90-thin.dream3d"
-    path = "/Users/jameslamb/Documents/research/data/CoNi90-thin/CoNi90-thin_merged.dream3d"
-    cell_data_path = "DataContainers/ImageDataContainer/CellData"
-    spacing_path = "DataContainers/ImageDataContainer/_SIMPL_GEOMETRY/SPACING"
-    dream3d_nx = False
 
-    # Burgers vector magnitude in m
-    burgers = 2.48e-10
-    # Number of CPU cores to use
-    n_cpus = 12
-    # Crystal structure, 1 = FCC, 2 = BCC, 3 = HCP
-    cs = 1
-    # "l2" or "l1" (where l1 is the absolute value); l2 is faster, l1 may be more accurate
-    minimization = "l2"
-    progress_bar = True
-    # How many data points to process in one chunk (decrease if memory issues)
-    chunk_size = 1000
-    # Spacing units
-    units = "um"
-
+    # Read in the data from the DREAM3D file
     euler, ids = utils.read_dream3d(
         path,
         ids_path=f"{cell_data_path}/FeatureIds",
@@ -42,6 +46,7 @@ if __name__ == "__main__":
     spacing = utils.read_dream3d_spacing(
         path, spacing_path=spacing_path, dream3d_nx=dream3d_nx
     )
+    # Convert spacing to meters
     if units == "nm" or units == "nanometer" or units == "nanometers":
         spacing *= 1e-9
     elif (
@@ -60,11 +65,11 @@ if __name__ == "__main__":
     else:
         raise ValueError("units must be one of 'nm', 'um', 'mm', or 'm'")
 
-    calc = False
+    # Get densities of geometrically necessary dislocations
     if calc:
-        dd1, mis1 = GND.calculate(
-            euler[:101],
-            ids[:101],
+        dd, mis = GND.calculate(
+            euler,
+            ids,
             cs,
             burgers,
             spacing,
@@ -73,56 +78,15 @@ if __name__ == "__main__":
             progress_bar,
             chunk_size,
         )
-        np.save("dd1.npy", dd1[minimization])
-        np.save("mis1.npy", mis1)
-
-        dd2, mis2 = GND.calculate(
-            euler[99:],
-            ids[99:],
-            cs,
-            burgers,
-            spacing,
-            minimization,
-            n_cpus,
-            progress_bar,
-            chunk_size,
-        )
-        np.save("dd2.npy", dd2[minimization])
-        np.save("mis2.npy", mis2)
+        np.save("dd.npy", dd[minimization])
+        np.save("mis.npy", mis)
+        print("Finished calculations")
     else:
         print("Reading in calculated data from .npy files")
-        dd1 = np.load("dd1.npy")
-        mis1 = np.load("mis1.npy")
-        dd2 = np.load("dd2.npy")
-        mis2 = np.load("mis2.npy")
-
-    dd = {
-        minimization: np.empty(
-            (dd1.shape[0], dd1.shape[1] + dd2.shape[1] - 2, dd1.shape[2], dd1.shape[3]),
-            dtype=dd1.dtype,
-        )
-    }
-    dd[minimization][:, : dd1.shape[1] - 1] = dd1[:, :-1]
-    dd[minimization][:, dd1.shape[1] - 1 :] = dd2[:, 1:]
-    del dd1, dd2  # Free memory
-
-    # print("dd1:", dd1.shape, "mis1:", mis1.shape)
-    # print("dd2:", dd2.shape, "mis2:", mis2.shape)
-    # print("dd:", dd[minimization].shape)
-    # print("mis:", mis.shape)
-
-    mis = np.empty(
-        (
-            mis1.shape[0],
-            mis1.shape[1] + mis2.shape[1] - 2,
-            mis1.shape[2],
-            mis1.shape[3],
-        ),
-        dtype=mis1.dtype,
-    )
-    mis[:, : mis1.shape[1] - 1] = mis1[:, :-1]
-    mis[:, mis1.shape[1] - 1 :] = mis2[:, 1:]
-    del mis1, mis2  # Free memory
+        dd = np.load("dd.npy")
+        mis = np.load("mis.npy")
+        dd = {minimization: dd}
+        print("Finished reading in data")
 
     # Save the results back to the DREAM3D file
     import h5py
