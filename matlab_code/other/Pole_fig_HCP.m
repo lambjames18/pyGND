69871535e8a875e8cf8def068fc2e778a75c8dd0
James Lamb <lambjames18@gmail.com>
2025-12-17 10:38:35 -0800

    Removing MATLAB code

--- matlab_code/other/Pole_fig_HCP.m
+++
@@ -1,7 +0,0 @@
-fname = 'D:\IMMI Work\FFT_Inputs\FFT_Ti7_1pct_r2_REDUX.txt';
-cs = crystalSymmetry.load('Ti-Titanium-alpha.cif');
-ori1 = orientation.load(fname,'columnNames',{'phi1','Phi','phi2','x','y','z'},cs);
-odf1 = calcDensity(ori1);
-h = [Miller(0,0,0,1,cs,'uvtw'),Miller(1,0,-1,0,cs,'uvtw'),Miller(2,-1,-1,0,cs,'uvtw')];
-setMTEXpref('xAxisDirection','east');setMTEXpref('zAxisDirection','outOfPlane');
-figure;plotPDF(odf1,h,'antipodal','projection','eangle','contourf','minmax');
