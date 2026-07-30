defba51b1524c2c0a7b70c9faeba503a4be4ec55
James Douglas Lamb <jdlamb@sandia.gov>
2026-07-30 15:31:21 -0700

    Putting function back in.

--- src/pygnd/__init__.py
+++ src/pygnd/__init__.py
@@ -7,10 +7,11 @@ densities from EBSD data using Nye's dislocation theory.
 
 from pygnd._version import __version__
 
-from pygnd.core import calculate_and_save, get_linear_operator
+from pygnd.core import calculate_and_save
 from pygnd import core, io, rotations, quaternions, utils
 
 __all__ = [
+    "calculate_and_save",
     "core",
     "io",
     "rotations",
